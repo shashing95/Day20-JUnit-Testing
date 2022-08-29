8e5de48413369fe813895f0fb8d8d29c9237f157
shashi <shashidharngg@gmail.com>
2022-08-29 23:50:48 +0530

    added:Program for the test uc of first name

---
+++ src/com/bridgrlabz/UserRegistration.java
@@ -0,0 +1,16 @@
+package com.bridgrlabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	public boolean firstName(String firstName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+	}
+
+}
+
+

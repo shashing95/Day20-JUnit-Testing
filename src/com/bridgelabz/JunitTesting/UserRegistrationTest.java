f99a4a1e7a3f8f527d4987c061e499fb2f19768c
shashi <shashidharngg@gmail.com>
2022-08-31 23:35:40 +0530

    added:Program for the test last name validation

--- src/com/bridgelabz/JunitTesting/UserRegistrationTest.java
+++ src/com/bridgelabz/JunitTesting/UserRegistrationTest.java
@@ -9,17 +9,17 @@ class UserRegistrationTest {
 
 	@Test
 	void test() {
-		UserRegistrationJ userRegistration = new UserRegistrationJ();
+		UserRegistration userRegistration = new UserRegistration();
 
 		@Test
 		public void givenFirstName_WhenProper_ShouldReturnTrue() {
-			boolean result = userRegistration.firstName("Xyz");
+			boolean result = UserRegistration.firstName("Xyz");
 			Assert.assertEquals(true, result);
 		}
 
 		@Test
 		public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
-			boolean result = userRegistration.firstName("Xyz");
+			boolean result = UserRegistration.firstName("Xyz");
 			Assert.assertEquals(false, result);
 		
 	
---
+++ src/com/bridgrlabz/UserRegistrationJ.java
@@ -0,0 +1,24 @@
+package com.bridgrlabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationJ {
+
+	public boolean firstName(String firstName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+	}
+
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
+	}
+
+}
+
+
